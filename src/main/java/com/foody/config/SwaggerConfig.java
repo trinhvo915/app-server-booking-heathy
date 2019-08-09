c08a2d721ae7099a6fd15efa86dfa1d8b0246637
trinhvovan <trinh.vo@orientsoftware.com>
2019-08-09 11:37:19 +0700

    fix add token into swagger

--- src/main/java/com/foody/config/SwaggerConfig.java
+++ src/main/java/com/foody/config/SwaggerConfig.java
@@ -4,9 +4,12 @@ import com.google.common.base.Predicate;
 import com.google.common.base.Predicates;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
@@ -14,9 +17,12 @@ import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
 import springfox.documentation.service.Contact;
 import springfox.documentation.service.Parameter;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -26,7 +32,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
-	 @Bean
+	@Bean
     public Docket api() {
 		 //Adding Header
 	        ParameterBuilder aParameterBuilder = new ParameterBuilder();
@@ -64,6 +70,21 @@ public class SwaggerConfig {
 	    return new ApiKey("authkey", "Authorization", "header");
 	}
 
+    @SuppressWarnings("unused")
+	private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .forPaths(PathSelectors.any()).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope(
+                "global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return Arrays.asList(new SecurityReference("Bearer",
+                authorizationScopes));
+    }
+    
     // Only select apis that matches the given Predicates.
     @SuppressWarnings("unused")
 	private Predicate<String> paths() {
