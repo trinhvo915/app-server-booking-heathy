918557d0d17472364378a6e30da73f75bf5b7dd2
trinhvovan <vovantrinhdhbkdn@gmail.com>
2019-10-03 17:18:13 +0700

    add get token code to register to become doctor

--- src/main/java/com/foody/repository/ExpertCodeRepository.java
+++ src/main/java/com/foody/repository/ExpertCodeRepository.java
@@ -1,9 +1,13 @@
 package com.foody.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.foody.entities.ExpertCode;
 
 public interface ExpertCodeRepository extends JpaRepository<ExpertCode,String>{
-
+	
+	@Query(value ="Select * from expert_code e where e.token_code = :tokenCode", nativeQuery=true)
+	ExpertCode getExpertCode(@Param("tokenCode") Integer tokenCode);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,9 +1,8 @@
-INSERT INTO `role` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'USER');
-INSERT INTO `role` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'ADMIN');
-INSERT INTO `role` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'EXPERT');
-
-INSERT INTO `degree` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'BÁC SỸ');
-INSERT INTO `degree` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'THẠC SỸ SỸ');
-INSERT INTO `degree` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'TIẾN SỸ');
-INSERT INTO `degree` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'PHÓ GIÁO SƯ');
-INSERT INTO `degree` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'GIÁO SƯ');
+INSERT INTO `role` (`id`,`created_at`,`update_at`,`is_active`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'1','USER');
+INSERT INTO `role` (`id`,`created_at`,`update_at`,`is_active`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'1','ADMIN');
+INSERT INTO `role` (`id`,`created_at`,`update_at`,`is_active`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'1','EXPERT');
+INSERT INTO `degree` (`id`,`created_at`,`update_at`,`is_active`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'1','BÁC SỸ');
+INSERT INTO `degree` (`id`,`created_at`,`update_at`,`is_active`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'1','THẠC SỸ');
+INSERT INTO `degree` (`id`,`created_at`,`update_at`,`is_active`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'1','TIẾN SỸ');
+INSERT INTO `degree` (`id`,`created_at`,`update_at`,`is_active`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'1','PHÓ GIÁO SƯ');
+INSERT INTO `degree` (`id`,`created_at`,`update_at`,`is_active`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'1','GIÁO SƯ');
