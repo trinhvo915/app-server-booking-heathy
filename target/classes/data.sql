9be7b15486684172b47a6d86bc4e043c1aaf2311
trinhvovan <vovantrinhdhbkdn@gmail.com>
2019-10-03 09:16:29 +0700

    add database image

---
+++ target/classes/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO `role` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'USER');
+INSERT INTO `role` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'ADMIN');
+INSERT INTO `role` (`id`,`created_at`,`update_at`,`name`) VALUES (UUID(),CURDATE(),CURDATE(),'EXPERT');
